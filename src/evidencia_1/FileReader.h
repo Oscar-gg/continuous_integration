87d87adec016f86c58c17a55b52fe444a44e8653
Oscar Arreola <oscar.arreola.jr@gmail.com>
2024-11-16 01:13:24 -0600

    refactor: pre-commit hook changes

--- src/evidencia_1/FileReader.h
+++ src/evidencia_1/FileReader.h
@@ -1,26 +1,29 @@
 #ifndef FILE_READER
 #define FILE_READER
 
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <sstream>
-#include <string> 
+#include <string>
 
 using namespace std;
 
-class FileReader {
-    public:
-        static string readFile(string path) {
-            ifstream file(path);
-                if (!file) {
-                    cout << "Error opening file: " << path << endl;
-                    return "";
-                };
+class FileReader
+{
+public:
+    static string readFile(string path)
+    {
+        ifstream file(path);
+        if (!file)
+        {
+            cout << "Error opening file: " << path << endl;
+            return "";
+        };
 
-                ostringstream ss;
-                ss << file.rdbuf();  
-                return ss.str(); 
-        }
+        ostringstream ss;
+        ss << file.rdbuf();
+        return ss.str();
+    }
 };
 
 #endif // FILE_READER
--- src/evidencia_1/Kmp.h
+++ src/evidencia_1/Kmp.h
@@ -9,7 +9,7 @@ using namespace std;
 namespace algorithms
 {
 
-    /*
+/*
         KMP algorithm to check if a text is contained in anotherone
         Params: text (string that could contain a pattern)
                 pattern (string that could be contained in text)
@@ -17,60 +17,60 @@ namespace algorithms
         Returns: vector (positions where the pattern is contained int text)
         Complexity: O(m + n)
     */
-    vector<int> containsText(string &text, string &pattern, vector<int> &lps)
+vector<int> containsText(string &text, string &pattern, vector<int> &lps)
+{
+    vector<int> result;
+    int i = 0, j = 0;
+    while (i < text.length())
     {
-        vector<int> result;
-        int i = 0, j = 0;
-        while (i < text.length())
+        if (text[i] == pattern[j])
         {
-            if (text[i] == pattern[j])
+            i++;
+            j++;
+            if (j == pattern.length())
             {
-                i++;
-                j++;
-                if (j == pattern.length())
-                {
-                    result.push_back(i - pattern.length());
-                }
-            }
-            else if (j == 0)
-            {
-                i++;
-            }
-            else
-            {
-                j = lps[j - 1];
+                result.push_back(i - pattern.length());
             }
         }
-        return result;
+        else if (j == 0)
+        {
+            i++;
+        }
+        else
+        {
+            j = lps[j - 1];
+        }
     }
+    return result;
+}
 
-    /*
+/*
         Algorithm to obtain the longest proper prefix vector
         Params: text (string to be preprocessed)
         Returns: vector (vector with longest prefix values)
         Complexity: O(n)
     */
-    vector<int> preprocess(string text)
+vector<int> preprocess(string text)
+{
+    vector<int> lps(text.size(), 0);
+    int j = 0;
+
+    for (int i = 1; i < text.size(); i++)
     {
-        vector<int> lps(text.size(), 0);
-        int j = 0;
-
-        for (int i = 1; i < text.size(); i++)
+        if (text[i] == text[j])
         {
-            if (text[i] == text[j])
-            {
-                lps[i] = ++j;
-            }
-            else if (j > 0)
-            {
-                j = lps[j - 1];
-                i--;
-            }
+            lps[i] = ++j;
         }
-
-        return lps;
+        else if (j > 0)
+        {
+            j = lps[j - 1];
+            i--;
+        }
     }
 
-};
+    return lps;
+}
+
+}; // namespace algorithms
 
 #endif // KMP
